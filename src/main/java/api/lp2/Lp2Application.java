0dcc5babb5138e8163cecb485eb759db122e30c0
ribeirin-7 <arthurr366@gmail.com>
2024-12-11 13:21:20 -0300

    Sistema de Pagamento

---
+++ src/main/java/api/lp2/Lp2Application.java
@@ -0,0 +1,13 @@
+package api.lp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/api/lp2/Lp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package api.lp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
