b4ef784e8f4dc7de368bb485ab5ec5bcd647d4e1
ribeirin-7 <arthurr366@gmail.com>
2024-12-13 11:40:02 -0300

    Criação da Tabela Transações

---
+++ src/main/java/api/lp2/models/StatusPagamento.java
@@ -0,0 +1,8 @@
+package api.lp2.models;
+
+public enum StatusPagamento {
+    PENDENTE,
+    APROVADO,
+    FALHOU,
+    REEMBOLSADO
+}
---
+++ src/main/java/api/lp2/models/Transacao.java
@@ -0,0 +1,75 @@
+package api.lp2.models;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+
+
+//Tabela de transação
+
+@Entity
+@Table(name = Transacao.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Transacao {
+
+    public static final String TABLE_NAME = "transacoes";   
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    private User user;
+
+
+    @Positive
+    @NotNull
+    @NotEmpty
+    @Column(name = "valor", nullable = false)
+    private double valor;
+
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    @NotEmpty
+    private StatusPagamento status;
+
+    @NotNull
+    @NotEmpty
+    @Column(name = "data", nullable = false)
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime data;
+
+    @NotNull
+    @NotEmpty
+    @Column(name = "metodo_de_pagamento", nullable = false)
+    private String metodo_de_pagamento;
+
+
+
+}
